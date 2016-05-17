91a35d0c05bada787211ab2289d29fffa3588d60
Jonatan Souza <jonatangd.souza@gmail.com>
2016-05-17 12:42:02 -0300

    better identity by uncrustify

--- arquivos_invertidos/index.c
+++ arquivos_invertidos/index.c
@@ -1,5 +1,6 @@
 //TODO
-//  colocar a lista pra imprimir em ordem alfabetica
+//  esta colocando a primeira palavra por ultimo.. pq??
+
 //TODO
 
 
@@ -15,12 +16,12 @@ char TEXT[50];
 Idx* ROOT = NULL;
 
 struct index {
-        char key[17];
-        int *value;
-        int n;
-        struct index* next;
-        struct index* listRight;
-        struct index* listLeft;
+	char key[17];
+	int *value;
+	int n;
+	struct index* next;
+	struct index* listRight;
+	struct index* listLeft;
 };
 
 //AUXILIAR FUNCTIONS
@@ -34,75 +35,75 @@ void pushLinkedList(Idx* list, Idx* node);
 //AUXILIAR FUNCTIONS
 
 int index_createfrom(const char *key_file, const char *text_file, Index **idx){
-        int i;
-        (*idx) = malloc(sizeof(Index));
-        if((*idx) == NULL) {
-                return 1;
-        }
-        if (getKeyWordsFromFile(key_file, text_file, idx)) {
-                return 1;
-        }
+	int i;
+	(*idx) = malloc(sizeof(Index));
+	if((*idx) == NULL) {
+		return 1;
+	}
+	if (getKeyWordsFromFile(key_file, text_file, idx)) {
+		return 1;
+	}
 
-        return 0;
+	return 0;
 }
 
 int index_get( const Index *idx, const char *key, int **occurrences, int *num_occurrences ){
-        int addr = hash(key);
-        Idx *node = (*idx)[addr];
-        if(node == NULL) {
-                return 1;
-        }else{
-                do {
-                        if (!strcmp(node->key, key)) {
-                                (*occurrences) = node->value;
-                                *num_occurrences = node->n;
-                                return 0;
-                        }
-                        node = node->next;
-                } while(node->next != NULL);
-        }
+	int addr = hash(key);
+	Idx *node = (*idx)[addr];
+	if(node == NULL) {
+		return 1;
+	}else{
+		do {
+			if (!strcmp(node->key, key)) {
+				(*occurrences) = node->value;
+				*num_occurrences = node->n;
+				return 0;
+			}
+			node = node->next;
+		} while(node->next != NULL);
+	}
 
-        return 1;
+	return 1;
 }
 int index_put( Index *idx, const char *key ){
-        Idx* node;
-        int addr = hash(key);
-        FILE *fp;
-        fp = fopen (TEXT,"r");
-        if (fp==NULL)
-        {
-                return 1;
-        }
-        node = checkOccurrencesOnText(key, fp);
-        if(node == NULL) {
-                printf("nenhuma ocorrencia de %s no texto\n", key);
-        }else{
-                pushLinkedList(ROOT, node);
-                if((*idx)[addr] == NULL) {
-                        (*idx)[addr] = node;
-                }else{
-                        node->next = (*idx)[addr]->next;
-                        (*idx)[addr]->next = node;
-                }
-        }
-        fclose (fp);
-        return 0;
+	Idx* node;
+	int addr = hash(key);
+	FILE *fp;
+	fp = fopen (TEXT,"r");
+	if (fp==NULL)
+	{
+		return 1;
+	}
+	node = checkOccurrencesOnText(key, fp);
+	if(node == NULL) {
+		printf("nenhuma ocorrencia de %s no texto\n", key);
+	}else{
+		pushLinkedList(ROOT, node);
+		if((*idx)[addr] == NULL) {
+			(*idx)[addr] = node;
+		}else{
+			node->next = (*idx)[addr]->next;
+			(*idx)[addr]->next = node;
+		}
+	}
+	fclose (fp);
+	return 0;
 }
 int index_print( const Index *idx ){
 
-        Idx *head = NULL, *list = NULL, *tmp = NULL;
-        int i;
-        tmp = ROOT;
-        while(tmp != NULL) {
-                char occurrences[tmp->n];
-                printf("%s: ", tmp->key);
-                for(i = 0; i < (tmp->n-1); i++)
-                        printf("%d, ", tmp->value[i]);
+	Idx *head = NULL, *list = NULL, *tmp = NULL;
+	int i;
+	tmp = ROOT;
+	while(tmp != NULL) {
+		char occurrences[tmp->n];
+		printf("%s: ", tmp->key);
+		for(i = 0; i < (tmp->n-1); i++)
+			printf("%d, ", tmp->value[i]);
 
-                printf("%d\n", tmp->value[tmp->n-1]);
-                tmp = tmp->listRight;
-        }
-        return 0;
+		printf("%d\n", tmp->value[tmp->n-1]);
+		tmp = tmp->listRight;
+	}
+	return 0;
 }
 
 //********************//
@@ -110,152 +111,152 @@ int index_print( const Index *idx ){
 //********************//
 
 static int hash(const char *key){
-        unsigned int hash = 5183;
-        int c;
-        while (c = *key++)
-                hash = ((hash << 5) + hash) + c;
-        return hash % M;
+	unsigned int hash = 5183;
+	int c;
+	while (c = *key++)
+		hash = ((hash << 5) + hash) + c;
+	return hash % M;
 }
 
 int getKeyWordsFromFile(const char* keys, const char* text, Index **idx){
-        Idx* node;
-        FILE *fk, *ft;
-        char str[17];
-        int c, wordSizeControl = 0;
-        fk = fopen(keys, "r");
-        if(fk == NULL) {
-                printf("Erro ao abrir o arquivo de chaves\n");
-                return 1;
-        }
-        ft = fopen(text, "r");
-        if(ft == NULL) {
-                printf("Erro ao abrir o arquivo de texto\n");
-                return 1;
-        }
+	Idx* node;
+	FILE *fk, *ft;
+	char str[17];
+	int c, wordSizeControl = 0;
+	fk = fopen(keys, "r");
+	if(fk == NULL) {
+		printf("Erro ao abrir o arquivo de chaves\n");
+		return 1;
+	}
+	ft = fopen(text, "r");
+	if(ft == NULL) {
+		printf("Erro ao abrir o arquivo de texto\n");
+		return 1;
+	}
 
-        strcpy(TEXT, text);
+	strcpy(TEXT, text);
 
-        while ((c = fgetc(fk)) != EOF) {
-                if (c == '\n') {
-                        str[wordSizeControl] = '\0';
-                        wordSizeControl = 0;
-                        node = checkOccurrencesOnText(str, ft);
-                        if(node == NULL) {
-                                printf("nenhuma ocorrencia no texto da chave\n");
-                        }else{
+	while ((c = fgetc(fk)) != EOF) {
+		if (c == '\n') {
+			str[wordSizeControl] = '\0';
+			wordSizeControl = 0;
+			node = checkOccurrencesOnText(str, ft);
+			if(node == NULL) {
+				printf("nenhuma ocorrencia no texto da chave\n");
+			}else{
 
-                                if(ROOT == NULL) {
-                                        ROOT = node;
-                                        ROOT->listLeft = NULL;
-                                        ROOT->listRight = NULL;
-                                }else{
-                                        pushLinkedList(ROOT, node);
-                                }
+				if(ROOT == NULL) {
+					ROOT = node;
+					ROOT->listLeft = NULL;
+					ROOT->listRight = NULL;
+				}else{
+					pushLinkedList(ROOT, node);
+				}
 
-                                if((**idx)[hash(str)] == NULL) {
-                                        (**idx)[hash(str)] = node;
-                                }else{
-                                        node->next = (**idx)[hash(str)]->next;
-                                        (**idx)[hash(str)]->next = node;
-                                }
-                        }
-                } else if(wordSizeControl < 16) {
-                        str[wordSizeControl++] = setLowerCase(c);
-                }
-        }
-        fclose(ft);
-        fclose(fk);
-        return 0;
+				if((**idx)[hash(str)] == NULL) {
+					(**idx)[hash(str)] = node;
+				}else{
+					node->next = (**idx)[hash(str)]->next;
+					(**idx)[hash(str)]->next = node;
+				}
+			}
+		} else if(wordSizeControl < 16) {
+			str[wordSizeControl++] = setLowerCase(c);
+		}
+	}
+	fclose(ft);
+	fclose(fk);
+	return 0;
 }
 
 void pushLinkedList(Idx* list, Idx* node){
-        Idx* aux;
-        while(list->listRight != NULL) {
-                if(strcmp(node->key, list->key) < strcmp(list->key, node->key)) {
-                        if (list->listLeft == NULL) {
-                                ROOT = node;
-                        }
-                        aux = list->listLeft;
-                        list->listLeft = node;
-                        node->listRight = list;
-                        if(aux != NULL) {
-                                node->listLeft = aux;
-                                aux->listRight = node;
-                        }
+	Idx* aux;
+	while(list->listRight != NULL) {
+		if(strcmp(node->key, list->key) < strcmp(list->key, node->key)) {
+			if (list->listLeft == NULL) {
+				ROOT = node;
+			}
+			aux = list->listLeft;
+			list->listLeft = node;
+			node->listRight = list;
+			if(aux != NULL) {
+				node->listLeft = aux;
+				aux->listRight = node;
+			}
 
-                        return;
-                }else if(strcmp(node->key, list->key) == strcmp(list->key, node->key)) {
-                        return;
-                }
-                list= list->listRight;
-        }
+			return;
+		}else if(strcmp(node->key, list->key) == strcmp(list->key, node->key)) {
+			return;
+		}
+		list= list->listRight;
+	}
 
-        if (list->listLeft == NULL) {
-                ROOT = node;
-        }
-        aux = list->listLeft;
-        list->listLeft = node;
-        node->listRight = list;
-        if(aux != NULL) {
-                aux->listRight = node;
-                node->listLeft = aux;
-        }
+	if (list->listLeft == NULL) {
+		ROOT = node;
+	}
+	aux = list->listLeft;
+	list->listLeft = node;
+	node->listRight = list;
+	if(aux != NULL) {
+		aux->listRight = node;
+		node->listLeft = aux;
+	}
 }
 
 int validateSearch(int n, char* text){
-        if(text[-1] < 'A'  ||  text[-1] > 'z')
-                if (text[n] < 'A'  ||  text[n] > 'z')
-                        return 1;
-        return 0;
+	if(text[-1] < 'A'  ||  text[-1] > 'z')
+		if (text[n] < 'A'  ||  text[n] > 'z')
+			return 1;
+	return 0;
 }
 
 int setLowerCase(int c){
-        if ((c >= 'A') && (c <= 'Z'))
-                c = c + 32;
-        return c;
+	if ((c >= 'A') && (c <= 'Z'))
+		c = c + 32;
+	return c;
 }
 
 Idx* checkOccurrencesOnText(const char *key, FILE *fp){
-        Idx *node;
-        int *val = NULL;
-        int c, line = 1, lineDelimiter = 0, occurrences = 0;
-        char text[10000];
-        char *position;
-        while ((c = setLowerCase(fgetc(fp))) != EOF) {
-                if(c == '\n') {
-                        text[lineDelimiter] = '\0';
-                        position = strstr(text, key);
-                        if(position != NULL) {
-                                if(validateSearch((int)strlen(key), position)) {
-                                        if(val == NULL) {
-                                                val = malloc(sizeof(int));
-                                        }else{
-                                                if(realloc(val, (occurrences+1)*sizeof(int)) == NULL) {
-                                                        printf("memoria cheia\n");
-                                                }
-                                        }
-                                        val[occurrences] = line;
-                                        occurrences++;
-                                }
-                        }
-                        position = NULL;
-                        lineDelimiter = 0;
-                        line++;
-                }else{
-                        text[lineDelimiter++] = c;
-                }
-        }
-        if(val != NULL) {
-                node = malloc(sizeof(Idx));
-                node->value = val;
-                node->n = occurrences;
-                strcpy(node->key, key);
-                node->next=NULL;
-                node->listRight = NULL;
-                node->listLeft = NULL;
-                rewind(fp);
-                return node;
-        }
-        rewind(fp);
-        return NULL;
+	Idx *node;
+	int *val = NULL;
+	int c, line = 1, lineDelimiter = 0, occurrences = 0;
+	char text[10000];
+	char *position;
+	while ((c = setLowerCase(fgetc(fp))) != EOF) {
+		if(c == '\n') {
+			text[lineDelimiter] = '\0';
+			position = strstr(text, key);
+			if(position != NULL) {
+				if(validateSearch((int)strlen(key), position)) {
+					if(val == NULL) {
+						val = malloc(sizeof(int));
+					}else{
+						if(realloc(val, (occurrences+1)*sizeof(int)) == NULL) {
+							printf("memoria cheia\n");
+						}
+					}
+					val[occurrences] = line;
+					occurrences++;
+				}
+			}
+			position = NULL;
+			lineDelimiter = 0;
+			line++;
+		}else{
+			text[lineDelimiter++] = c;
+		}
+	}
+	if(val != NULL) {
+		node = malloc(sizeof(Idx));
+		node->value = val;
+		node->n = occurrences;
+		strcpy(node->key, key);
+		node->next=NULL;
+		node->listRight = NULL;
+		node->listLeft = NULL;
+		rewind(fp);
+		return node;
+	}
+	rewind(fp);
+	return NULL;
 }
--- matrizes_esparsas/matrix.c
+++ matrizes_esparsas/matrix.c
@@ -7,11 +7,11 @@
 #define INPUT_MAX_SIZE 1000
 
 struct matrix {
-        struct matrix *right;
-        struct matrix *below;
-        int line;
-        int column;
-        float info;
+	struct matrix *right;
+	struct matrix *below;
+	int line;
+	int column;
+	float info;
 };
 
 //AUXILIAR FUNCTIONS
@@ -23,205 +23,205 @@ void matrix_print_line(Matrix *m);
 
 
 int matrix_create(Matrix **m){
-        int i;
-        float matrixDescription[INPUT_MAX_SIZE];
+	int i;
+	float matrixDescription[INPUT_MAX_SIZE];
 
-        int matrixDescriptionSize=0;
-        for(i=0; i< INPUT_MAX_SIZE; i++) {
-                scanf("%f", &matrixDescription[matrixDescriptionSize]);
-                if(matrixDescription[matrixDescriptionSize] == 0)
-                        break;
-                matrixDescriptionSize++;
+	int matrixDescriptionSize=0;
+	for(i=0; i< INPUT_MAX_SIZE; i++) {
+		scanf("%f", &matrixDescription[matrixDescriptionSize]);
+		if(matrixDescription[matrixDescriptionSize] == 0)
+			break;
+		matrixDescriptionSize++;
 
-        }
+	}
 
 
-        const int LINES =(int) matrixDescription[0];
-        const int COLUMNS =(int) matrixDescription[1];
+	const int LINES =(int) matrixDescription[0];
+	const int COLUMNS =(int) matrixDescription[1];
 
-        float * matrixElements = matrixDescription+2;
-        const int matrixElementsSize = matrixDescriptionSize-2;
+	float * matrixElements = matrixDescription+2;
+	const int matrixElementsSize = matrixDescriptionSize-2;
 
-        create_estructure(m, LINES, COLUMNS);
+	create_estructure(m, LINES, COLUMNS);
 
-        for(i=0; i < (matrixElementsSize/3); i++)
-                insert_element((*m), matrixElements[(i*3)],matrixElements[(i*3)+1], matrixElements[(i*3)+2]);
+	for(i=0; i < (matrixElementsSize/3); i++)
+		insert_element((*m), matrixElements[(i*3)],matrixElements[(i*3)+1], matrixElements[(i*3)+2]);
 
-        return 0;
+	return 0;
 }
 
 int matrix_getelem( const Matrix* m, int x, int y, float *elem ){
-        Matrix *head = (Matrix *) m;
-        int i;
+	Matrix *head = (Matrix *) m;
+	int i;
 //set Line
-        for(i=0; i < x; i++)
-                head = head->below;
+	for(i=0; i < x; i++)
+		head = head->below;
 
-        int found = 0;
-        Matrix *aux;
-        for(aux = head->right; aux != head; aux = aux->right) {
-                if(aux->line == x && aux->column == y) {
-                        *elem = aux->info;
-                        found = 1;
-                        break;
-                }
-        }
+	int found = 0;
+	Matrix *aux;
+	for(aux = head->right; aux != head; aux = aux->right) {
+		if(aux->line == x && aux->column == y) {
+			*elem = aux->info;
+			found = 1;
+			break;
+		}
+	}
 
-        if(found) {
-                return 0;
-        }
-        *elem = 0;
-        return 1;
+	if(found) {
+		return 0;
+	}
+	*elem = 0;
+	return 1;
 }
 
 int matrix_setelem( Matrix* m, int x, int y, float elem ){
-        Matrix *head = (Matrix *) m;
-        int i;
-        //set Line
-        for(i=0; i < x; i++)
-                head = head->below;
+	Matrix *head = (Matrix *) m;
+	int i;
+	//set Line
+	for(i=0; i < x; i++)
+		head = head->below;
 
-        int found = 0;
-        Matrix *aux;
-        for(aux = head->right; aux != head; aux = aux->right) {
-                if(aux->line == x && aux->column == y) {
-                        found = 1;
-                        break;
-                }
-        }
+	int found = 0;
+	Matrix *aux;
+	for(aux = head->right; aux != head; aux = aux->right) {
+		if(aux->line == x && aux->column == y) {
+			found = 1;
+			break;
+		}
+	}
 
-        if(found) {
-                aux->info = elem;
-                return 0;
-        }else
-                return 1;
+	if(found) {
+		aux->info = elem;
+		return 0;
+	}else
+		return 1;
 
 }
 
 int matrix_destroy( Matrix* m ){
-        Matrix *aux = m;
-        Matrix *ref;
-        if(m == NULL)
-                return 0;
-        if(m->below == m) {
-                destroy_line(m);
-                return 0;
-        }
-        ref = aux;
-        while(aux->below != m) {
-                ref = aux;
-                aux = aux->below;
-        }
+	Matrix *aux = m;
+	Matrix *ref;
+	if(m == NULL)
+		return 0;
+	if(m->below == m) {
+		destroy_line(m);
+		return 0;
+	}
+	ref = aux;
+	while(aux->below != m) {
+		ref = aux;
+		aux = aux->below;
+	}
 
-        destroy_line(aux);
-        ref->below = m;
-        matrix_destroy(m);
+	destroy_line(aux);
+	ref->below = m;
+	matrix_destroy(m);
 
 }
 
 
 
 int matrix_print( const Matrix* m ){
-        Matrix *aux = m->below;
-        printf("********** MATRIX %d X %d **********\n", m->line,
-               m->column);
-        while(aux != m) {
-                matrix_print_line(aux);
-                aux = aux->below;
-        }
-        printf("\n");
+	Matrix *aux = m->below;
+	printf("********** MATRIX %d X %d **********\n", m->line,
+	       m->column);
+	while(aux != m) {
+		matrix_print_line(aux);
+		aux = aux->below;
+	}
+	printf("\n");
 }
 
 int matrix_add( const Matrix* m, const Matrix* n, Matrix** r ){
 
-        float inputs[INPUT_MAX_SIZE];
-        int i, j, LINE, COLUMN, inputsSize = 0;
-        float infoA, infoB;
-        if(m->line != n->line || m->column != n->column)
-                return 1;
+	float inputs[INPUT_MAX_SIZE];
+	int i, j, LINE, COLUMN, inputsSize = 0;
+	float infoA, infoB;
+	if(m->line != n->line || m->column != n->column)
+		return 1;
 
-        LINE = m->line;
-        COLUMN = m->column;
+	LINE = m->line;
+	COLUMN = m->column;
 
-        create_estructure(r, LINE, COLUMN);
-        for(i=0; i<LINE; i++) {
-                for(j=0; j<COLUMN; j++) {
-                        matrix_getelem(m, i+1, j+1, &infoA);
-                        matrix_getelem(n, i+1, j+1, &infoB);
-                        //printf("matrix %d,%d, %f\n",i+1, j+1, infoA);
-                        if(infoA != 0 || infoB !=0) {
-                                inputs[inputsSize++] = i+1;
-                                inputs[inputsSize++] = j+1;
-                                inputs[inputsSize++] = infoA+infoB;
-                        }
-                }
-        }
-        for(i=0; i<inputsSize/3; i++) {
-                insert_element((*r), (int)inputs[i*3], (int)inputs[i*3+1], inputs[i*3+2]);
-        }
-        return 0;
+	create_estructure(r, LINE, COLUMN);
+	for(i=0; i<LINE; i++) {
+		for(j=0; j<COLUMN; j++) {
+			matrix_getelem(m, i+1, j+1, &infoA);
+			matrix_getelem(n, i+1, j+1, &infoB);
+			//printf("matrix %d,%d, %f\n",i+1, j+1, infoA);
+			if(infoA != 0 || infoB !=0) {
+				inputs[inputsSize++] = i+1;
+				inputs[inputsSize++] = j+1;
+				inputs[inputsSize++] = infoA+infoB;
+			}
+		}
+	}
+	for(i=0; i<inputsSize/3; i++) {
+		insert_element((*r), (int)inputs[i*3], (int)inputs[i*3+1], inputs[i*3+2]);
+	}
+	return 0;
 }
 
 int matrix_multiply( const Matrix* m, const Matrix* n, Matrix** r ){
-        float inputs[INPUT_MAX_SIZE];
-        int i, j, k, LINE, COLUMN, ELEMENTS, inputsSize = 0;
-        float infoA, infoB, result = 0;
-        if(m->column != n->line)
-                return 1;
-        LINE = m->line;
-        COLUMN = n->column;
-        ELEMENTS = m->line;
+	float inputs[INPUT_MAX_SIZE];
+	int i, j, k, LINE, COLUMN, ELEMENTS, inputsSize = 0;
+	float infoA, infoB, result = 0;
+	if(m->column != n->line)
+		return 1;
+	LINE = m->line;
+	COLUMN = n->column;
+	ELEMENTS = m->line;
 
-        create_estructure(r, LINE, COLUMN);
+	create_estructure(r, LINE, COLUMN);
 
-        for(i=0; i<LINE; i++) {
-                for(j=0; j<COLUMN; j++) {
-                        for(k=0; k < ELEMENTS; k++) {
-                                matrix_getelem(m, i+1, k+1, &infoA);
-                                matrix_getelem(n, k+1, j+1, &infoB);
-                                result += infoA * infoB;
-                        }
-                        if(result) {
-                                inputs[inputsSize++] = i+1;
-                                inputs[inputsSize++] = j+1;
-                                inputs[inputsSize++] = result;
-                        }
-                        result = 0;
-                }
-        }
+	for(i=0; i<LINE; i++) {
+		for(j=0; j<COLUMN; j++) {
+			for(k=0; k < ELEMENTS; k++) {
+				matrix_getelem(m, i+1, k+1, &infoA);
+				matrix_getelem(n, k+1, j+1, &infoB);
+				result += infoA * infoB;
+			}
+			if(result) {
+				inputs[inputsSize++] = i+1;
+				inputs[inputsSize++] = j+1;
+				inputs[inputsSize++] = result;
+			}
+			result = 0;
+		}
+	}
 
-        for(i=0; i<inputsSize/3; i++) {
-                insert_element((*r), (int)inputs[i*3], (int)inputs[i*3+1], inputs[i*3+2]);
-        }
-        return 0;
+	for(i=0; i<inputsSize/3; i++) {
+		insert_element((*r), (int)inputs[i*3], (int)inputs[i*3+1], inputs[i*3+2]);
+	}
+	return 0;
 
 }
 
 int matrix_transpose( const Matrix* m, Matrix** r ){
-        float inputs[INPUT_MAX_SIZE];
-        int i, j, LINE, COLUMN, inputsSize = 0;
-        float info;
+	float inputs[INPUT_MAX_SIZE];
+	int i, j, LINE, COLUMN, inputsSize = 0;
+	float info;
 
-        LINE = m->column;
-        COLUMN = m->line;
+	LINE = m->column;
+	COLUMN = m->line;
 
-        create_estructure(r, LINE, COLUMN);
+	create_estructure(r, LINE, COLUMN);
 
-        for(i=0; i < COLUMN; i++) {
-                for(j=0; j<LINE; j++ ) {
-                        matrix_getelem(m, i+1, j+1, &info);
-                        if(info) {
-                                inputs[inputsSize++] = j+1;
-                                inputs[inputsSize++] = i+1;
-                                inputs[inputsSize++] = info;
-                        }
-                }
-        }
+	for(i=0; i < COLUMN; i++) {
+		for(j=0; j<LINE; j++ ) {
+			matrix_getelem(m, i+1, j+1, &info);
+			if(info) {
+				inputs[inputsSize++] = j+1;
+				inputs[inputsSize++] = i+1;
+				inputs[inputsSize++] = info;
+			}
+		}
+	}
 
-        for(i=0; i<inputsSize/3; i++) {
-                insert_element((*r), (int)inputs[i*3], (int)inputs[i*3+1], inputs[i*3+2]);
-        }
-        return 0;
+	for(i=0; i<inputsSize/3; i++) {
+		insert_element((*r), (int)inputs[i*3], (int)inputs[i*3+1], inputs[i*3+2]);
+	}
+	return 0;
 
 }
 
@@ -230,91 +230,91 @@ int matrix_transpose( const Matrix* m, Matrix** r ){
 //********************//
 
 void create_estructure(Matrix **m, int LINES, int COLUMNS){
-        int i;
-        //MasterHeadNode
-        (*m) = malloc (sizeof (Matrix));
-        (*m)->right = (*m);
-        (*m)->below = (*m);
-        (*m)->line = LINES;
-        (*m)->column = COLUMNS;
-        (*m)->info = FLT_MIN;
+	int i;
+	//MasterHeadNode
+	(*m) = malloc (sizeof (Matrix));
+	(*m)->right = (*m);
+	(*m)->below = (*m);
+	(*m)->line = LINES;
+	(*m)->column = COLUMNS;
+	(*m)->info = FLT_MIN;
 
-        //create LINES Heads
-        for(i=0; i<LINES; i++) {
-                Matrix *mNew = malloc(sizeof (Matrix));
-                mNew->column = -1;
-                mNew->line = 0;
-                mNew->info = FLT_MIN;
-                mNew->right = mNew;
-                mNew->below = (*m)->below;
-                (*m)->below = mNew;
-        }
+	//create LINES Heads
+	for(i=0; i<LINES; i++) {
+		Matrix *mNew = malloc(sizeof (Matrix));
+		mNew->column = -1;
+		mNew->line = 0;
+		mNew->info = FLT_MIN;
+		mNew->right = mNew;
+		mNew->below = (*m)->below;
+		(*m)->below = mNew;
+	}
 
-        //create COLUMNS Heads
-        for(i=0; i<COLUMNS; i++) {
-                Matrix *mNew = malloc(sizeof (Matrix));
-                mNew->line = -1;
-                mNew->column = 0;
-                mNew->info = FLT_MIN;
-                mNew->below = mNew;
-                mNew->right = (*m)->right;
-                (*m)->right = mNew;
-        }
+	//create COLUMNS Heads
+	for(i=0; i<COLUMNS; i++) {
+		Matrix *mNew = malloc(sizeof (Matrix));
+		mNew->line = -1;
+		mNew->column = 0;
+		mNew->info = FLT_MIN;
+		mNew->below = mNew;
+		mNew->right = (*m)->right;
+		(*m)->right = mNew;
+	}
 }
 
 void matrix_print_line(Matrix *m){
-        Matrix *aux = m->right;
-        while(aux != m) {
-                printf("%d , ", aux->line);
-                printf("%d, ", aux->column);
-                printf("info %f\n", aux->info);
-                aux = aux->right;
-        }
+	Matrix *aux = m->right;
+	while(aux != m) {
+		printf("%d , ", aux->line);
+		printf("%d, ", aux->column);
+		printf("info %f\n", aux->info);
+		aux = aux->right;
+	}
 }
 
 void insert_element(Matrix *m, int line, int column, float info){
-        Matrix *aux = m;
-        Matrix *ref;
-        int i;
+	Matrix *aux = m;
+	Matrix *ref;
+	int i;
 
 //new node
-        Matrix *mNew = (Matrix *) malloc(sizeof(Matrix));
-        mNew->info = info;
-        mNew->column = column;
-        mNew->line = line;
+	Matrix *mNew = (Matrix *) malloc(sizeof(Matrix));
+	mNew->info = info;
+	mNew->column = column;
+	mNew->line = line;
 //set Line
-        for(i=0; i<line; i++)
-                aux = aux->below;
+	for(i=0; i<line; i++)
+		aux = aux->below;
 
-        ref = aux;
-        while(aux->right != ref)
-                aux = aux->right;
+	ref = aux;
+	while(aux->right != ref)
+		aux = aux->right;
 
-        mNew->right = aux->right;
-        aux->right = mNew;
+	mNew->right = aux->right;
+	aux->right = mNew;
 //set column
 //set origin
-        aux = m;
+	aux = m;
 
-        for(i=0; i<column; i++)
-                aux = aux->right;
+	for(i=0; i<column; i++)
+		aux = aux->right;
 
-        ref = aux;
-        while(aux->below != ref)
-                aux = aux->below;
+	ref = aux;
+	while(aux->below != ref)
+		aux = aux->below;
 
-        mNew->below = aux->below;
-        aux->below = mNew;
+	mNew->below = aux->below;
+	aux->below = mNew;
 
 
 }
 
 void destroy_line(Matrix *m){
-        Matrix *aux = m, *trash;
-        while(aux->right != m) {
-                trash = aux;
-                aux = aux->right;
-                free(trash);
-        }
-        free(aux);
+	Matrix *aux = m, *trash;
+	while(aux->right != m) {
+		trash = aux;
+		aux = aux->right;
+		free(trash);
+	}
+	free(aux);
 }
