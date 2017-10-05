d60723fee73775d7b02b363a4ae72381f409b8f0
jonatan souza <jonatangd.souza@gmail.com>
2017-10-05 14:10:44 -0300

    paginator errors handle

--- parallel_computing/paginator1/main.c
+++ parallel_computing/paginator1/main.c
@@ -6,9 +6,33 @@
  #include <unistd.h>
  #include <sys/wait.h>
 
-int main(int argc, char **args){
+ #define N_ARGS 2
+
+void help_messages(){
+    printf("Failed: incorrect number of args\n");
+    printf("Usage: ./paginator <path_file> \n");
+    exit(-1);
+}
+
+void error_message(){
+			perror("error: ");
+      exit(-1);
+}
+
+int main(int argc, char **argv){
 
 	int fd[2], pid;
+	FILE *file;
+
+  if(argc != N_ARGS){
+    help_messages();
+  }
+
+	file = fopen(argv[1], "r");
+
+	if(file == NULL){
+			error_message();
+	}
 
 	char *paginator[2] = {"/bin/more", NULL};
 	pipe(fd);
@@ -20,22 +44,21 @@ int main(int argc, char **args){
 		close(fd[1]);
 		execv(paginator[0], paginator);
 
-	}else{
-		close(STDOUT_FILENO);
+  }else if(pid < 0){
+    error_message();
+  }else{
+
+  	close(STDOUT_FILENO);
 		dup(fd[1]);
 		close(fd[0]);
 		close(fd[1]);
 		char c;
-		FILE *file;
-		file = fopen("main.c", "r");
-		if (file) {
-			while ((c = (char) getc(file)) != EOF)
-				write(1, &c, sizeof(c));
-			fclose(file);
-		}
+		while ((c = (char) getc(file)) != EOF)
+			write(1, &c, sizeof(c));
+		fclose(file);
 		close(1);
 	}
 	waitpid(pid, NULL, 0);
-	
+
 	return 0;
 }
