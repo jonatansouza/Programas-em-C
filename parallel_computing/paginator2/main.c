828dc81cbfe73af6c57f6bd2d70d2a600e6ceea5
Jonatan Gall <jonatangd.souza@gmail.com>
2017-10-12 18:21:03 -0300

    paginator finalized

--- parallel_computing/paginator2/main.c
+++ parallel_computing/paginator2/main.c
@@ -1,36 +1,75 @@
 /**
-   @author Jonatan Souza
- */
+*   Trabalho da disciplina Programação Concorrente e Sistemas Operacionais
+* 	Professora Raquel
+*
+*	@author Jonatan Souza
+*   
+*   Este programa recebe como argumento o path de um arquivo 
+*   e repassa linha a linha para um processo filho 
+*   que executa o paginador more
+*/
+
  #include <stdio.h>
  #include <stdlib.h>
  #include <unistd.h>
  #include <sys/wait.h>
 
-int main(int argc, char **args){
+ #define N_ARGS 2
+
+void help_messages(){
+    printf("Failed: incorrect number of args\n");
+    printf("Usage: ./paginator <path_file> \n");
+    exit(-1);
+}
+
+void error_message(){
+    perror("error: ");
+    exit(-1);
+}
+
+int main(int argc, char **argv){
 
 	int fd[2], pid;
+	FILE *file;
+
+  	if(argc != N_ARGS){
+    		help_messages();
+  	}
+
+	file = fopen(argv[1], "r");
+
+	if(file == NULL){
+		error_message();
+	}
 
 	char *paginator[2] = {"/bin/more", NULL};
+	
 	pipe(fd);
 
 	if ((pid = fork()) == 0) {
-		dup2(fd[0], 0);
+		
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
 		close(fd[1]);
 		execv(paginator[0], paginator);
 
+  	}else if(pid < 0){
+    
+		error_message();
+  	
 	}else{
-		dup2(fd[1], 1);
-		close(fd[0]);
+
 		char c;
-		FILE *file;
-		file = fopen("main.c", "r");
-		if (file) {
-			while ((c = (char) getc(file)) != EOF)
-				write(1, &c, sizeof(c));
-			fclose(file);
-		}
-		close(1);
+	
+		while ((c = (char) getc(file)) != EOF)
+			write(fd[1], &c, sizeof(c));
+		
+		fclose(file);
+		close(fd[0]);
+		close(fd[1]);
 	}
+	
 	waitpid(pid, NULL, 0);
-  return 0;
+
+	return 0;
 }
